4e535aa4485cdfca9e2c071cdb56c98537c2f074
majed29c <majedshmaitlu@gmail.com>
2025-04-26 01:29:44 +0300

    small edits

--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -14,27 +14,9 @@ import { book } from "@/actions/book";
 import BookedAppointments from "@/components/bookedappointments/BookedAppointments";
 import { getInfo } from "@/actions/getInfo";
 import cookie from "js-cookie";
-
-
+import { services } from "./services"; // Import services
 
 const Appointment = () => {
-  const services = [
-    { title: "General Check-ups & Vaccinations", description: "Routine health exams and vaccinations." },
-    { title: "Emergency & Surgery", description: "Immediate medical attention and surgical procedures." },
-    { title: "Dental Care & Diagnostics", description: "Professional teeth cleaning, extractions, and oral health check-ups." },
-    { title: "Parasite Prevention & Microchipping", description: "Protection against fleas, ticks, and worms." },
-    { title: "Bathing & Grooming", description: "Professional pet washing, fur trimming, and nail clipping." },
-    { title: "Skin & Coat Treatments", description: "Specialized care for allergies, excessive shedding, and skin infections." },
-    { title: "Ear Cleaning & Nail Trimming", description: "Essential hygiene services to prevent ear infections." },
-    { title: "Physical Therapy & Recovery", description: "Rehabilitation exercises and treatments for pets." },
-    { title: "Laser Therapy & Acupuncture", description: "Alternative pain relief and healing methods." },
-    { title: "Overnight Stays", description: "Safe boarding options for pets." },
-    { title: "Daycare Programs", description: "Supervised play and socialization programs." },
-    { title: "Pet Training Sessions", description: "Basic obedience, behavioral training, and problem-solving sessions." },
-    { title: "Nutritional Consultations", description: "Expert guidance on pet diets to ensure optimal health." },
-    { title: "Compassionate Euthanasia", description: "Humane end-of-life care for aging or terminally ill pets." },
-    { title: "Memorial Services", description: "Support and options for honoring and remembering pets." }
-  ];
   const [success, setSuccess] = useState(false);
   const [message, setMessage] = useState("");
   const [availableTimes, setAvailableTimes] = useState<string[]>([]);
@@ -46,7 +28,7 @@ const Appointment = () => {
     date: "",
     time: "",
     service: "",
-    species: "", 
+    species: "",
   });
 
   useEffect(() => {
